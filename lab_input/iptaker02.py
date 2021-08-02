be2530c1498a48b9cabc482af05c9748333edd37
AaronElizondo <88336570+AaronElizondo@users.noreply.github.com>
2021-08-02 19:45:36 +0000

    learning about input statements

---
+++ lab_input/iptaker02.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+user_input=input("Please enter an IPv4 address:")
+
+## the line below creates a sing string that is passed to print()
+# print("You told me the IPv4 address is:\t" +user_input)
+
+## print() can be given a series of objects separated by a comma
+print("You told me the IPv4 address is:\t", user_input)
+
+user_company=input("What company do you work for?")
+print("so you work for:", user_company)
+
+print("have a good day at ", user_company)
